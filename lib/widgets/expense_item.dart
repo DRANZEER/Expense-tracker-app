d46038517c591755ce2a20209f3f542e28cd73e5
DRANZEER <sujalshaw00@gmail.com>
2025-03-06 23:29:17 +0530

    adding date

--- lib/widgets/expense_item.dart
+++ lib/widgets/expense_item.dart
@@ -20,8 +20,14 @@ class ExpenseItem extends StatelessWidget {
             const SizedBox(height: 4),
             Row(
               children: [
-                Text(
-                  '\$${expense.amount.toStringAsFixed(2)}',
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(Icons.alarm),
+                    const SizedBox(width: 8),
+                    Text(expense.date.toString()),
+                  ],
                 ),
               ],
             ),
