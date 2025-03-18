c8b4ca1da4b7ea457b0f6cbed0edeb4553a2504c
DRANZEER <sujalshaw00@gmail.com>
2025-03-18 12:35:49 +0530

    app completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
@@ -11,81 +12,86 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        cardTheme: CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
+  // WidgetsFlutterBinding.ensureInitialized();  // this is for to lock orientation of the App.
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) {
+    runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
+          cardTheme: CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
+            ),
+          ),
+          textTheme: TextTheme().copyWith(
+            bodyMedium: TextStyle(
+              color: kDarkColorScheme.onSecondaryContainer,
+            ),
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kDarkColorScheme.onSecondaryContainer,
+              fontSize: 16,
+            ),
+            titleSmall: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kDarkColorScheme.onSecondaryContainer,
+            ),
+            titleMedium: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kDarkColorScheme.onPrimaryContainer,
+            ),
           ),
         ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        theme: ThemeData().copyWith(
+          //scaffoldBackgroundColor: Color.fromARGB(255, 151, 168, 233),
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: TextTheme().copyWith(
+            bodyMedium: TextStyle(
+              color: kColorScheme.onSecondaryContainer,
+            ),
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 16,
+            ),
+            titleSmall: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kColorScheme.onSecondaryContainer,
+            ),
+            titleMedium: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kColorScheme.onPrimaryContainer,
+            ),
           ),
         ),
-         textTheme: TextTheme().copyWith(
-          bodyMedium: TextStyle(
-            color: kDarkColorScheme.onSecondaryContainer,
-          ),
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: kDarkColorScheme.onSecondaryContainer,
-            fontSize: 16,
-          ),
-          titleSmall: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: kDarkColorScheme.onSecondaryContainer,
-          ),
-          titleMedium: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: kDarkColorScheme.onPrimaryContainer,
-          ),
-        ),
+        themeMode: ThemeMode.system,
+        home: const Expenses(),
       ),
-      theme: ThemeData().copyWith(
-        //scaffoldBackgroundColor: Color.fromARGB(255, 151, 168, 233),
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: TextTheme().copyWith(
-          bodyMedium: TextStyle(
-            color: kColorScheme.onSecondaryContainer,
-          ),
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: kColorScheme.onSecondaryContainer,
-            fontSize: 16,
-          ),
-          titleSmall: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: kColorScheme.onSecondaryContainer,
-          ),
-          titleMedium: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: kColorScheme.onPrimaryContainer,
-          ),
-        ),
-      ),
-      themeMode: ThemeMode.system,
-      home: const Expenses(),
-    ),
-  );
+    );
+  // });
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -31,6 +31,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpenses(onAddExpenses: _addExpense),
@@ -68,6 +69,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     Widget mainContent = const Center(
       child: Text(
         'No expenses found. Start adding somae!',
@@ -93,12 +95,21 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(expenses: _registeredExpenses),
+                ),
+                Expanded(child: mainContent),
+              ],
+            ),
     );
   }
 }
